3609ec3ddce66d7f321606ed27fc67283b8499a8
ExpressPath <pdx.error@gmail.com>
2026-05-10 14:10:33 +0900

    Use fuel recursion for Coq CIC JSON export

--- tools/coq-cic-metarocq-template.v
+++ tools/coq-cic-metarocq-template.v
@@ -75,188 +75,200 @@ Definition ivucx_json_projection (proj : projection) : string :=
     ivucx_json_field "arg" (string_of_nat (proj_arg proj))
   ].
 
-Fixpoint ivucx_json_term (t : term) : string :=
-  match t with
-  | tRel n =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "rel");
-        ivucx_json_field "index" (string_of_nat n)
-      ]
-  | tVar id =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "var");
-        ivucx_json_field "name" (ivucx_json_string id)
-      ]
-  | tEvar ev args =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "evar");
-        ivucx_json_field "index" (string_of_nat ev);
-        ivucx_json_field "args" ("[" +s ivucx_json_term_items args +s "]")
-      ]
-  | tSort s =>
-      ivucx_json_sort s
-  | tCast tm kind ty =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "cast");
-        ivucx_json_field "castKind" (ivucx_json_cast_kind kind);
-        ivucx_json_field "term" (ivucx_json_term tm);
-        ivucx_json_field "type" (ivucx_json_term ty)
-      ]
-  | tProd na ty body =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "prod");
-        ivucx_json_field "name" (ivucx_json_name (binder_name na));
-        ivucx_json_field "binderInfo" (ivucx_json_string "default");
-        ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
-        ivucx_json_field "type" (ivucx_json_term ty);
-        ivucx_json_field "body" (ivucx_json_term body)
-      ]
-  | tLambda na ty body =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "lambda");
-        ivucx_json_field "name" (ivucx_json_name (binder_name na));
-        ivucx_json_field "binderInfo" (ivucx_json_string "default");
-        ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
-        ivucx_json_field "type" (ivucx_json_term ty);
-        ivucx_json_field "body" (ivucx_json_term body)
-      ]
-  | tLetIn na def def_ty body =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "let");
-        ivucx_json_field "name" (ivucx_json_name (binder_name na));
-        ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
-        ivucx_json_field "type" (ivucx_json_term def_ty);
-        ivucx_json_field "value" (ivucx_json_term def);
-        ivucx_json_field "body" (ivucx_json_term body);
-        ivucx_json_field "nondep" (ivucx_json_bool false)
-      ]
-  | tApp f args =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "app");
-        ivucx_json_field "fn" (ivucx_json_term f);
-        ivucx_json_field "args" ("[" +s ivucx_json_term_items args +s "]")
-      ]
-  | tConst c u =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "const");
-        ivucx_json_field "name" (ivucx_json_string (string_of_kername c));
-        ivucx_json_field "universes" (ivucx_json_instance u)
-      ]
-  | tInd ind u =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "ind");
-        ivucx_json_field "name" (ivucx_json_string (string_of_inductive ind));
-        ivucx_json_field "universes" (ivucx_json_instance u)
-      ]
-  | tConstruct ind idx u =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "construct");
-        ivucx_json_field "inductive" (ivucx_json_string (string_of_inductive ind));
-        ivucx_json_field "ctorIndex" (string_of_nat idx);
-        ivucx_json_field "universes" (ivucx_json_instance u)
-      ]
-  | tCase ci type_info discr branches =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "case");
-        ivucx_json_field "inductive" (ivucx_json_string (string_of_inductive (ci_ind ci)));
-        ivucx_json_field "caseInfo" (
+Definition ivucx_json_truncated : string :=
+  ivucx_json_object [
+    ivucx_json_field "kind" (ivucx_json_string "truncated")
+  ].
+
+Fixpoint ivucx_json_term_fuel (fuel : nat) (t : term) {struct fuel} : string :=
+  match fuel with
+  | O => ivucx_json_truncated
+  | S fuel' =>
+      let fix ivucx_json_term_items (items : list term) : string :=
+        match items with
+        | [] => ""
+        | [item] => ivucx_json_term_fuel fuel' item
+        | item :: rest => ivucx_json_term_fuel fuel' item +s "," +s ivucx_json_term_items rest
+        end in
+      let fix ivucx_json_branch_items (items : list (branch term)) : string :=
+        match items with
+        | [] => ""
+        | [item] =>
+            ivucx_json_object [
+              ivucx_json_field "names" (ivucx_json_aname_array (bcontext item));
+              ivucx_json_field "body" (ivucx_json_term_fuel fuel' (bbody item))
+            ]
+        | item :: rest =>
+            ivucx_json_object [
+              ivucx_json_field "names" (ivucx_json_aname_array (bcontext item));
+              ivucx_json_field "body" (ivucx_json_term_fuel fuel' (bbody item))
+            ] +s "," +s ivucx_json_branch_items rest
+        end in
+      let fix ivucx_json_def_items (items : list (def term)) : string :=
+        match items with
+        | [] => ""
+        | [item] =>
+            ivucx_json_object [
+              ivucx_json_field "name" (ivucx_json_name (binder_name (dname item)));
+              ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance (dname item))));
+              ivucx_json_field "type" (ivucx_json_term_fuel fuel' (dtype item));
+              ivucx_json_field "body" (ivucx_json_term_fuel fuel' (dbody item));
+              ivucx_json_field "recursiveArg" (string_of_nat (rarg item))
+            ]
+        | item :: rest =>
+            ivucx_json_object [
+              ivucx_json_field "name" (ivucx_json_name (binder_name (dname item)));
+              ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance (dname item))));
+              ivucx_json_field "type" (ivucx_json_term_fuel fuel' (dtype item));
+              ivucx_json_field "body" (ivucx_json_term_fuel fuel' (dbody item));
+              ivucx_json_field "recursiveArg" (string_of_nat (rarg item))
+            ] +s "," +s ivucx_json_def_items rest
+        end in
+      match t with
+      | tRel n =>
           ivucx_json_object [
-            ivucx_json_field "npars" (string_of_nat (ci_npar ci));
-            ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (ci_relevance ci)))
+            ivucx_json_field "kind" (ivucx_json_string "rel");
+            ivucx_json_field "index" (string_of_nat n)
           ]
-        );
-        ivucx_json_field "predicate" (
+      | tVar id =>
           ivucx_json_object [
-            ivucx_json_field "universes" (ivucx_json_instance (puinst type_info));
-            ivucx_json_field "params" ("[" +s ivucx_json_term_items (pparams type_info) +s "]");
-            ivucx_json_field "context" (ivucx_json_aname_array (pcontext type_info));
-            ivucx_json_field "returnType" (ivucx_json_term (preturn type_info))
+            ivucx_json_field "kind" (ivucx_json_string "var");
+            ivucx_json_field "name" (ivucx_json_string id)
           ]
-        );
-        ivucx_json_field "discriminant" (ivucx_json_term discr);
-        ivucx_json_field "branches" ("[" +s ivucx_json_branch_items branches +s "]")
-      ]
-  | tProj proj tm =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "proj");
-        ivucx_json_field "typeName" (ivucx_json_string (string_of_inductive (proj_ind proj)));
-        ivucx_json_field "index" (string_of_nat (proj_arg proj));
-        ivucx_json_field "projection" (ivucx_json_projection proj);
-        ivucx_json_field "struct" (ivucx_json_term tm)
-      ]
-  | tFix defs idx =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "fix");
-        ivucx_json_field "index" (string_of_nat idx);
-        ivucx_json_field "definitions" ("[" +s ivucx_json_def_items defs +s "]")
-      ]
-  | tCoFix defs idx =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "cofix");
-        ivucx_json_field "index" (string_of_nat idx);
-        ivucx_json_field "definitions" ("[" +s ivucx_json_def_items defs +s "]")
-      ]
-  | tInt _ =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "lit");
-        ivucx_json_field "literal" (ivucx_json_string "int");
-        ivucx_json_field "value" ivucx_json_null
-      ]
-  | tFloat _ =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "lit");
-        ivucx_json_field "literal" (ivucx_json_string "float");
-        ivucx_json_field "value" ivucx_json_null
-      ]
-  | tArray u items default_value item_type =>
-      ivucx_json_object [
-        ivucx_json_field "kind" (ivucx_json_string "array");
-        ivucx_json_field "universe" (ivucx_json_raw_level (string_of_level u));
-        ivucx_json_field "elements" ("[" +s ivucx_json_term_items items +s "]");
-        ivucx_json_field "default" (ivucx_json_term default_value);
-        ivucx_json_field "type" (ivucx_json_term item_type)
-      ]
-  end
-with ivucx_json_term_items (items : list term) : string :=
-  match items with
-  | [] => ""
-  | [item] => ivucx_json_term item
-  | item :: rest => ivucx_json_term item +s "," +s ivucx_json_term_items rest
-  end
-with ivucx_json_branch_items (items : list (branch term)) : string :=
-  match items with
-  | [] => ""
-  | [item] =>
-      ivucx_json_object [
-        ivucx_json_field "names" (ivucx_json_aname_array (bcontext item));
-        ivucx_json_field "body" (ivucx_json_term (bbody item))
-      ]
-  | item :: rest =>
-      ivucx_json_object [
-        ivucx_json_field "names" (ivucx_json_aname_array (bcontext item));
-        ivucx_json_field "body" (ivucx_json_term (bbody item))
-      ] +s "," +s ivucx_json_branch_items rest
-  end
-with ivucx_json_def_items (items : list (def term)) : string :=
-  match items with
-  | [] => ""
-  | [item] =>
-      ivucx_json_object [
-        ivucx_json_field "name" (ivucx_json_name (binder_name (dname item)));
-        ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance (dname item))));
-        ivucx_json_field "type" (ivucx_json_term (dtype item));
-        ivucx_json_field "body" (ivucx_json_term (dbody item));
-        ivucx_json_field "recursiveArg" (string_of_nat (rarg item))
-      ]
-  | item :: rest =>
-      ivucx_json_object [
-        ivucx_json_field "name" (ivucx_json_name (binder_name (dname item)));
-        ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance (dname item))));
-        ivucx_json_field "type" (ivucx_json_term (dtype item));
-        ivucx_json_field "body" (ivucx_json_term (dbody item));
-        ivucx_json_field "recursiveArg" (string_of_nat (rarg item))
-      ] +s "," +s ivucx_json_def_items rest
+      | tEvar ev args =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "evar");
+            ivucx_json_field "index" (string_of_nat ev);
+            ivucx_json_field "args" ("[" +s ivucx_json_term_items args +s "]")
+          ]
+      | tSort s =>
+          ivucx_json_sort s
+      | tCast tm kind ty =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "cast");
+            ivucx_json_field "castKind" (ivucx_json_cast_kind kind);
+            ivucx_json_field "term" (ivucx_json_term_fuel fuel' tm);
+            ivucx_json_field "type" (ivucx_json_term_fuel fuel' ty)
+          ]
+      | tProd na ty body =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "prod");
+            ivucx_json_field "name" (ivucx_json_name (binder_name na));
+            ivucx_json_field "binderInfo" (ivucx_json_string "default");
+            ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
+            ivucx_json_field "type" (ivucx_json_term_fuel fuel' ty);
+            ivucx_json_field "body" (ivucx_json_term_fuel fuel' body)
+          ]
+      | tLambda na ty body =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "lambda");
+            ivucx_json_field "name" (ivucx_json_name (binder_name na));
+            ivucx_json_field "binderInfo" (ivucx_json_string "default");
+            ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
+            ivucx_json_field "type" (ivucx_json_term_fuel fuel' ty);
+            ivucx_json_field "body" (ivucx_json_term_fuel fuel' body)
+          ]
+      | tLetIn na def def_ty body =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "let");
+            ivucx_json_field "name" (ivucx_json_name (binder_name na));
+            ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (binder_relevance na)));
+            ivucx_json_field "type" (ivucx_json_term_fuel fuel' def_ty);
+            ivucx_json_field "value" (ivucx_json_term_fuel fuel' def);
+            ivucx_json_field "body" (ivucx_json_term_fuel fuel' body);
+            ivucx_json_field "nondep" (ivucx_json_bool false)
+          ]
+      | tApp f args =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "app");
+            ivucx_json_field "fn" (ivucx_json_term_fuel fuel' f);
+            ivucx_json_field "args" ("[" +s ivucx_json_term_items args +s "]")
+          ]
+      | tConst c u =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "const");
+            ivucx_json_field "name" (ivucx_json_string (string_of_kername c));
+            ivucx_json_field "universes" (ivucx_json_instance u)
+          ]
+      | tInd ind u =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "ind");
+            ivucx_json_field "name" (ivucx_json_string (string_of_inductive ind));
+            ivucx_json_field "universes" (ivucx_json_instance u)
+          ]
+      | tConstruct ind idx u =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "construct");
+            ivucx_json_field "inductive" (ivucx_json_string (string_of_inductive ind));
+            ivucx_json_field "ctorIndex" (string_of_nat idx);
+            ivucx_json_field "universes" (ivucx_json_instance u)
+          ]
+      | tCase ci type_info discr branches =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "case");
+            ivucx_json_field "inductive" (ivucx_json_string (string_of_inductive (ci_ind ci)));
+            ivucx_json_field "caseInfo" (
+              ivucx_json_object [
+                ivucx_json_field "npars" (string_of_nat (ci_npar ci));
+                ivucx_json_field "relevance" (ivucx_json_string (string_of_relevance (ci_relevance ci)))
+              ]
+            );
+            ivucx_json_field "predicate" (
+              ivucx_json_object [
+                ivucx_json_field "universes" (ivucx_json_instance (puinst type_info));
+                ivucx_json_field "params" ("[" +s ivucx_json_term_items (pparams type_info) +s "]");
+                ivucx_json_field "context" (ivucx_json_aname_array (pcontext type_info));
+                ivucx_json_field "returnType" (ivucx_json_term_fuel fuel' (preturn type_info))
+              ]
+            );
+            ivucx_json_field "discriminant" (ivucx_json_term_fuel fuel' discr);
+            ivucx_json_field "branches" ("[" +s ivucx_json_branch_items branches +s "]")
+          ]
+      | tProj proj tm =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "proj");
+            ivucx_json_field "typeName" (ivucx_json_string (string_of_inductive (proj_ind proj)));
+            ivucx_json_field "index" (string_of_nat (proj_arg proj));
+            ivucx_json_field "projection" (ivucx_json_projection proj);
+            ivucx_json_field "struct" (ivucx_json_term_fuel fuel' tm)
+          ]
+      | tFix defs idx =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "fix");
+            ivucx_json_field "index" (string_of_nat idx);
+            ivucx_json_field "definitions" ("[" +s ivucx_json_def_items defs +s "]")
+          ]
+      | tCoFix defs idx =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "cofix");
+            ivucx_json_field "index" (string_of_nat idx);
+            ivucx_json_field "definitions" ("[" +s ivucx_json_def_items defs +s "]")
+          ]
+      | tInt _ =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "lit");
+            ivucx_json_field "literal" (ivucx_json_string "int");
+            ivucx_json_field "value" ivucx_json_null
+          ]
+      | tFloat _ =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "lit");
+            ivucx_json_field "literal" (ivucx_json_string "float");
+            ivucx_json_field "value" ivucx_json_null
+          ]
+      | tArray u items default_value item_type =>
+          ivucx_json_object [
+            ivucx_json_field "kind" (ivucx_json_string "array");
+            ivucx_json_field "universe" (ivucx_json_raw_level (string_of_level u));
+            ivucx_json_field "elements" ("[" +s ivucx_json_term_items items +s "]");
+            ivucx_json_field "default" (ivucx_json_term_fuel fuel' default_value);
+            ivucx_json_field "type" (ivucx_json_term_fuel fuel' item_type)
+          ]
+      end
   end.
+
+Definition ivucx_json_term (t : term) : string :=
+  ivucx_json_term_fuel 20000 t.
 
 Definition ivucx_json_constant_body (qualid_name : qualid) (body : constant_body) : string :=
   ivucx_json_object [
